785465a3e33bd2e5036dc5d51073b29c351cb17e
ANTONDrakon <ant-tu2012@yandex.ru>
2022-11-14 14:51:37 +0300

    one tu versa

--- viceversa/views.py
+++ viceversa/views.py
@@ -7,6 +7,11 @@ def index(request):
     return render(request, 'index.html')
 
 
-def reverse(request):    
-    return render(request, 'reverse.html')
+def reverse(request):
+    
+    user_text = request.GET['usertext']
+    
+    reversed_text = user_text[::-1]
+    
+    return render(request, 'reverse.html',{'usertext':user_text, 'reversedtext':reversed_text})
 

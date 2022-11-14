c39cc1502a67d8651fbac220c6c81552177ae56e
ANTONDrakon <ant-tu2012@yandex.ru>
2022-11-14 15:51:45 +0300

    one tu versad

--- viceversa/views.py
+++ viceversa/views.py
@@ -12,6 +12,9 @@ def reverse(request):
     user_text = request.GET['usertext']
     
     reversed_text = user_text[::-1]
+
+    number_of_words = len(user_text.split())
     
-    return render(request, 'reverse.html',{'usertext':user_text, 'reversedtext':reversed_text})
+    return render(request, 'reverse.html',{'usertext':user_text, 'reversedtext':reversed_text,
+                                          'numberofwords':number_of_words})
 
